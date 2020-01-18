4cd98a667ac25d8164b4ab63e6caa89283666ce8
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-01-18 10:48:54 -0500

    model mapping JSON response

--- StoreSearch/Models/SearchResult.swift
+++ StoreSearch/Models/SearchResult.swift
@@ -10,14 +10,27 @@ import Foundation
 
 class SearchResult: Codable, CustomStringConvertible {
     var description: String {
-       return "Name: \(name), Artist Name: \(artistName ?? "None")"
+        return "Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")\n"
     }
-    
     var trackName: String? = ""
     var artistName: String? = ""
-    
     var name: String {
         return trackName ?? ""
+    }
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL: String? = ""
+    var genre = ""
+    enum CodingKeys: String, CodingKey {
+      case imageSmall = "artworkUrl60"
+      case imageLarge = "artworkUrl100"
+      case storeURL = "trackViewUrl"
+      case genre = "primaryGenreName"
+      case kind, artistName, trackName
+      case trackPrice, currency
     }
 }
 
